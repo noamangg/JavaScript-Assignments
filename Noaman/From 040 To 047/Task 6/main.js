f63a309540195cc9dcfdb51fcf6df27d9ff8c48c
Noaman Grewaity <noamangg@gmail.com>
2023-01-11 23:29:57 +0300

    Week 6

--- Noaman/From 040 To 047/Task 6/main.js
+++ Noaman/From 040 To 047/Task 6/main.js
@@ -2,8 +2,7 @@ let arr1 = ["A", "C", "X"];
 let arr2 = ["D", "E", "F", "Y"];
 let allArrs = [];
 
-arr1.concat(arr2);
-arr1.sort();
-allArrs.unshift(arr1.pop(), arr1.pop(), arr1.pop())
+arr1 = arr1.concat(arr2).sort();
+allArrs.unshift(arr1.pop().toLowerCase(), arr1.pop().toLowerCase(), arr1.pop().toLowerCase())
 
 console.log(allArrs.reverse().join("")); // fxy
