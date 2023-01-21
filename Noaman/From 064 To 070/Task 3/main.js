d2ca83bbda920a1cad9914131d601e346243b710
Noaman Grewaity <noamangg@gmail.com>
2023-01-21 19:34:16 +0300

    e

--- Noaman/From 064 To 070/Task 3/main.js
+++ Noaman/From 064 To 070/Task 3/main.js
@@ -1,7 +1,7 @@
 let checker = (zName) => (status) => (salary) =>
   status === "Available"
     ? `${zName}, My Salary Is ${salary}`
-    : `Iam Not Avaialble`;
+    : `Iam Not Available`;
 
 console.log(checker("Osama")("Available")(4000)); // Osama, My Salary Is 4000
 console.log(checker("Ahmed")("Not Available")()); // Iam Not Avaialble
